6af6633e4c4431afdb765b31a7df8d54537ba5d8
Bing Wong <wbbady@outlook.com>
2022-04-07 10:39:51 +0800

    Update status.py

--- pages/status.py
+++ pages/status.py
@@ -14,7 +14,7 @@ templates = Jinja2Templates(directory='./templates')
 async def status_page(request: Request):
     return templates.TemplateResponse("status.html",{"request": request})
 
-# @cached(TTLCache(1024, 300)) #cache result for 300 seconds
+@cached(TTLCache(1024, 300)) #cache result for 300 seconds
 @status.get("/upptime", include_in_schema=False)
 async def upptime_page(request: Request):
     url = "https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/README.md"
@@ -24,6 +24,7 @@ async def upptime_page(request: Request):
     html = markdown.markdown(content, extensions=['markdown.extensions.tables'])
     html = html.replace('./','https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/')
     html = html.replace('https%3A%2F%2Fraw.githubusercontent.com%2Fchinobing%2Fupptime-rssinn%2FHEAD', 'https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/')
+    html = html.replace('<img alt="" src="https://favicons.githubusercontent.com/rssinn.com" height="13">','')
 
     return templates.TemplateResponse("upptime.html",{"request": request, "html":html})
 
