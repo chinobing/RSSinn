f0707c4183a18a3bede0b84613cffe6b0a506697
Bing Wong <wbbady@outlook.com>
2022-04-11 16:03:25 +0800

    add index page

--- pages/index.py
+++ pages/index.py
@@ -1,9 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import requests
+import markdown
 
 index = APIRouter()
+templates = Jinja2Templates(directory='./templates')
+
+# @index.get("/", include_in_schema=False)
+# async def home_page():
+#     response = RedirectResponse(url='/upptime')
+#     return response
+
 
 @index.get("/", include_in_schema=False)
-async def home_page():
-    response = RedirectResponse(url='/upptime')
-    return response
+async def home_page(request: Request):
+    summary_url = "https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/history/summary.json"
+    response = requests.get(summary_url)
+    up_down = response.json()
+    routes_status_up =len([True for x in up_down if x['status']=='up'])
+    routes_status_down = len([True for x in up_down if x['status'] == 'down'])
+
+    total_routes_numbers = len(up_down)
+
+
+    data = {}
+    data['total_routes_numbers'] = total_routes_numbers
+    data['routes_status_up'] = routes_status_up
+    data['routes_status_down'] = routes_status_down
+
+
+    return templates.TemplateResponse("index.html",{"request": request, "data":data})
--- pages/status.py
+++ pages/status.py
@@ -8,13 +8,13 @@ from cachetools import TTLCache
 
 status = APIRouter()
 templates = Jinja2Templates(directory='./templates')
-# status.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @status.get("/status", include_in_schema=False)
 async def status_page(request: Request):
     return templates.TemplateResponse("status.html",{"request": request})
 
-# @cached(TTLCache(1024, 300)) #cache result for 300 seconds
+
 @status.get("/upptime", include_in_schema=False)
 async def upptime_page(request: Request):
     url = "https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/README.md"
@@ -22,9 +22,10 @@ async def upptime_page(request: Request):
     content = response.text
     content = content.split('status page.')[1].split('[**Visit')[0]
     html = markdown.markdown(content, extensions=['markdown.extensions.tables'])
-    html = html.replace('./','https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/')
-    html = html.replace('https%3A%2F%2Fraw.githubusercontent.com%2Fchinobing%2Fupptime-rssinn%2FHEAD', 'https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/')
-    html = html.replace('<img alt="" src="https://favicons.githubusercontent.com/rssinn.com" height="13">','')
+    table = html.replace('./','https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/')
+    table = table.replace('https%3A%2F%2Fraw.githubusercontent.com%2Fchinobing%2Fupptime-rssinn%2FHEAD', 'https://cdn.jsdelivr.net/gh/chinobing/upptime-rssinn@master/')
+    table = table.replace('<img alt="" src="https://favicons.githubusercontent.com/rssinn.com" height="13">','')
+    table = table.replace('<table>', '<table class="table is-striped is-fullwidth">')
 
-    return templates.TemplateResponse("upptime.html",{"request": request, "html":html})
+    return templates.TemplateResponse("upptime.html",{"request": request, "table":table})
 
