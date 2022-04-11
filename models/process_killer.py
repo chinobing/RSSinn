3dd837ddda50e54e09ddc536d212545c20260fc5
Bing Wong <wbbady@outlook.com>
2022-04-12 00:40:11 +0800

    add js for navbar

--- models/process_killer.py
+++ models/process_killer.py
@@ -46,42 +46,45 @@ def kill_child_processes(parent_pid, sig=signal.SIGTERM):
     for process in children:
         process.send_signal(sig)
 
-def zombies_process_killer():
+        process.kill() #test needed
+    parent.kill() #test needed
+
+# def zombies_process_killer():
     # procObjList = [procObj for procObj in psutil.process_iter() if 'chrome' in procObj.name().lower()]
     # for elem in procObjList:
     #    print (elem)
-    kill_child_processes(os.getpid())
+    # kill_child_processes(os.getpid())
     # Find PIDs od all the running instances of process that contains 'chrome' in it's name
     # procObjList = [procObj for procObj in psutil.process_iter() if 'chrome' in procObj.name().lower() ]
     # for elem in procObjList:
     #    print (elem)
 
-# def zombies_process_killer():
-#     print("*** Check if a process is running or not ***")
-#     # Check if any chrome process was running or not.
-#     if checkIfProcessRunning('chrome'):
-#         print('Yes a chrome process was running')
-#     else:
-#         print('No chrome process was running')
-#     # print("*** Find PIDs of a running process by Name ***")
-#     # Find PIDs od all the running instances of process that contains 'chrome' in it's name
-#     listOfProcessIds = findProcessIdByName('chrome')
-#     if len(listOfProcessIds) > 0:
-#        print('Process Exists | PID and other details are')
-#        for elem in listOfProcessIds:
-#            processID = elem['pid']
-#            processName = elem['name']
-#            processCreationTime =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(elem['create_time']))
-#            create_time = datetime.strptime(processCreationTime, '%Y-%m-%d %H:%M:%S')
-#            now = datetime.now()
-#            delta = now - create_time
-#            if delta.seconds>=5:
-#                print(f'process_ID={processID}, process_Name={processName}, Created_Time {processCreationTime}, running for {delta.seconds} seconds. Process has been killed at {now}.')
-#                kill_child_processes(processID)
-#     else :
-#        print('No Running Process found with given text')
-#     # print('** Find running process by name using List comprehension **')
-#     # # Find PIDs od all the running instances of process that contains 'chrome' in it's name
-#     # procObjList = [procObj for procObj in psutil.process_iter() if 'chrome' in procObj.name().lower() ]
-#     # for elem in procObjList:
-#     #    print (elem)
+def zombies_process_killer():
+    print("*** Check if a process is running or not ***")
+    # Check if any chrome process was running or not.
+    if checkIfProcessRunning('chrome'):
+        print('Yes a chrome process was running')
+    else:
+        print('No chrome process was running')
+    # print("*** Find PIDs of a running process by Name ***")
+    # Find PIDs od all the running instances of process that contains 'chrome' in it's name
+    listOfProcessIds = findProcessIdByName('chrome')
+    if len(listOfProcessIds) > 0:
+       print('Process Exists | PID and other details are')
+       for elem in listOfProcessIds:
+           processID = elem['pid']
+           processName = elem['name']
+           processCreationTime =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(elem['create_time']))
+           create_time = datetime.strptime(processCreationTime, '%Y-%m-%d %H:%M:%S')
+           now = datetime.now()
+           delta = now - create_time
+           if delta.seconds>=1:
+               print(f'process_ID={processID}, process_Name={processName}, Created_Time {processCreationTime}, running for {delta.seconds} seconds. Process has been killed at {now}.')
+               kill_child_processes(processID)
+    else :
+       print('No Running Process found with given text')
+    # print('** Find running process by name using List comprehension **')
+    # # Find PIDs od all the running instances of process that contains 'chrome' in it's name
+    # procObjList = [procObj for procObj in psutil.process_iter() if 'chrome' in procObj.name().lower() ]
+    # for elem in procObjList:
+    #    print (elem)
