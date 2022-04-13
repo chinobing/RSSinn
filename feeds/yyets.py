63c7196d402f130be390ea2415af68eabab72bc5
Bing Wong <wbbady@outlook.com>
2022-04-14 00:06:35 +0800

    Update yyets.py

--- feeds/yyets.py
+++ feeds/yyets.py
@@ -7,9 +7,14 @@ from datetime import datetime
 from asyncache import cached
 from cachetools import TTLCache
 import json
+import re
 
 yyets = APIRouter()
 
+
+"""
+yyets 全站热搜  
+"""
 top_description=f"""
 - 作者： [@chinobing](https://github.com/chinobing/)
 
@@ -39,7 +44,7 @@ async def top():
     items_list = []
     for item in results:
         json_item = json.loads(item)
-        _rating = f"[{json_item['rating']}]分" if json_item['rating'] else ""
+        _rating = f"[{json_item['rating']}]分" if "rating" in json_item else ""
         title = f"{_rating}{json_item['name']}"
         img = json_item['posterLink']
         description = f"<img src='{img}'><br>{json_item['introduction']}<br><br>豆瓣地址：<a href=‘{json_item['doubanLink']}’>{title}</a>"
@@ -60,5 +65,51 @@ async def top():
     return RSSResponse(feed)
 
 
+"""
+yyets 评论区资源-阿里云盘
+"""
+discuss_description=f"""
+- 作者： [@chinobing](https://github.com/chinobing/)
 
+- 来源：`https://yyets.dmesg.app/discuss`
+- 参数：没有
+- 状态： {add_upptime_status('yyets-discuss')}
+"""
 
+@yyets.get("/discuss/",
+              summary="YYeTs-评论区资源-阿里云盘",
+              description=discuss_description)
+@cached(TTLCache(1024, 300)) #cache result for 300 seconds
+async def discuss():
+    url = 'https://yyets.dmesg.app/api/comment?resource_id=233&page=1&size=20'
+
+    fake = Faker()
+    FAKE_HEADERS = {'User-Agent':fake.user_agent()}
+
+    str_data = await fetch(url, headers=FAKE_HEADERS)
+    json_data = json.loads(str_data)
+    results = json_data['data']
+
+    if not results:
+        return
+
+    items_list = []
+    for item in results:
+        content = item['content']
+        if 'http' in content:
+            title = content.split('http')[0]
+            description = content
+            link = re.search(r"https://www.aliyundrive.com/s/(.*?)...........", content).group()
+            pub_date = datetime.strptime(item['date'], '%Y-%m-%d %H:%M:%S')
+
+            _item = Item(title=title, link=link, description=description, pub_date=pub_date)
+            items_list.append(_item)
+
+    feed_data = {
+        'title': 'YYeTs-评论区资源-阿里云盘',
+        'link': url,
+        'description': "",
+        'item': items_list,
+    }
+    feed = RSSFeed(**feed_data)
+    return RSSResponse(feed)
