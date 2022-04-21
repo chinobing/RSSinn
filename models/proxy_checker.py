189fd956fc1ea99f1b5a48cd3065e78b3906c662
Bing Wong <wbbady@outlook.com>
2022-04-21 13:10:20 +0800

    Update proxy_checker.py

--- models/proxy_checker.py
+++ models/proxy_checker.py
@@ -3,6 +3,8 @@ import requests
 from models.read_yaml import parsing_yaml
 proxy_pool_address = parsing_yaml()['fetch_proxy_settings']['proxy_pool']['server']
 
+
+
 class ProxyChecker:
     logger = logging.getLogger(__name__)
 
@@ -10,14 +12,14 @@ class ProxyChecker:
     def proxy(cls):
         proxy = requests.get(f"{proxy_pool_address}/get").json().get("proxy")
         try:
-            print('get proxy ...')
+            cls.logger.info('get proxy ...')
             ip = {"http": "http://" + proxy, "https": "https://" + proxy}
             r = requests.get("http://www.baidu.com", proxies=ip, timeout=4)
-            print(r.status_code)
+            cls.logger.info(r.status_code)
             if r.status_code == 200:
                 return proxy
         except:
-            print('get proxy again ...')
+            cls.logger.info('get proxy again ...')
             cls.delete_proxy(proxy)
             return cls.proxy()
 
